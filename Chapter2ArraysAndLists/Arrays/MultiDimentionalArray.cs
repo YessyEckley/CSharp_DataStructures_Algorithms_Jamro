a97f0c038b2e77f236dc681e1c1b78524113d38e
Yessy Eckley <YessyEckley@users.noreply.github.com>
2019-02-21 11:49:11 -0700

    Game Map
    Display for the game map example

--- Chapter2ArraysAndLists/Arrays/MultiDimentionalArray.cs
+++ Chapter2ArraysAndLists/Arrays/MultiDimentionalArray.cs
@@ -55,6 +55,18 @@ namespace Chapter2ArraysAndLists.Arrays
 			};
 
 			Console.OutputEncoding = UTF8Encoding.UTF8;
+			
+			for(int row = 0; row < map.GetLength(0); row++)
+			{
+				for(int column = 0; column < map.GetLength(1); column++)
+				{
+					Console.ForegroundColor = map[row, column].GetColor();
+					Console.Write(map[row, column).GetChar() + " ";
+				}
+				Console.WriteLine();
+			}
+			
+			Console.ForegroundCOlor = ConsoleColor.Gray;
 		}
     }
 }
