a612eaa14f33afeeb4326abca4ecc8d6a67ab8ed
Yessy Eckley <teameckleyfigueroa@gmail.com>
2019-02-21 22:00:01 -0700

    more constant updating

---
+++ DSAConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Chapter2ArraysAndLists.Arrays;
+
+namespace DSAConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // Hello World! Console App
+            // Console.WriteLine("Hello World!");
+            
+            // // Single dimnetion array - getting months
+            // var singleDimentionalArray = new SingleDimentionalArray();
+            // var months = singleDimentionalArray.MonthNames();
+            // Console.WriteLine(singleDimentionalArray.ArrayToString(months));
+            // var temp = new string[12];
+
+            // // Multidimentional array - Multiplication Table
+            // MultiDimentionalArray.MultiplicationTable();
+            // MultiDimentionalArray.MultiplicationTableToDisplay();
+
+            // Multidimentiona array - Game Map
+            MultiDimentionalArray.GameMapDisplay();
+        }
+    }
+}
